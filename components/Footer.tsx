12a15bd90d43ed79df68b160d20bbf54bfca8cc6
Abdelrhman0101 <abdalrhman.software.eg@gmail.com>
2025-10-26 02:23:30 +0300

    refactor(Footer): move Link outside list items for semantic correctness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -135,8 +135,8 @@ const Footer = () => {
         <div className={styles['footer-column']}>
           <motion.h3 className={styles['footer-title']} whileHover={{ scale: 1.02 }}>الدبلومات</motion.h3>
           <ul className={styles['footer-list']}>
-            <li className={styles['footer-item']}><Link href="/diplomas/baraa">بارع</Link></li>
-            <li className={styles['footer-item']}><Link href="/community">مجتمع أفق</Link></li>
+            <Link href="/diplomas/baraa"><li className={styles['footer-item']}>بارع</li></Link>
+            <Link href="/community"><li className={styles['footer-item']}>مجتمع أفق</li></Link>
           </ul>
         </div>
       </div>
